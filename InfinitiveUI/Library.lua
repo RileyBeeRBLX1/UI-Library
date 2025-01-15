086dbda7bd917aaa662cb13300683e8faff001c7
RileyBeeRBLX1 <rileybeerblx1@gmail.com>
2025-01-15 16:59:05 +0800

    Update Library.lua

--- InfinitiveUI/Library.lua
+++ InfinitiveUI/Library.lua
@@ -508,8 +508,7 @@ function CloseRS(time)
         Position = UDim2.new(0, Main.AbsolutePosition.X + (Main.AbsoluteSize.X / 2), 0, Main.AbsolutePosition.Y + (Main.AbsoluteSize.Y / 2))
     }):Play()
     wait(1)
-    Window.Visible = false
-    WindowBox.Visible = false
+    ScreenGui:Destroy()
     return ui_minimized
 end
 
